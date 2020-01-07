1810020daf14f854f08b8432912aefc2a6e4c063
sabean365 <beansarah365@gmail.com>
2020-01-07 12:46:57 -0500

    Add project files.

---
+++ Assignment3/StringCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Calculator
+{
+    public class StringCalculator
+    {
+
+        public int Add(string numbers)
+        {
+
+            if (numbers.Length == 0)
+                return 0;
+
+            int[] numbersArray = numbers.Split(',').Select(n => int.Parse(n)).ToArray();
+
+            CheckForNegative_ThrowException(numbersArray);
+
+            if (numbers.Length == 1)
+                return Int32.Parse(numbers);
+
+            if (numbersArray.Any(n => n > 1000))
+                return numbersArray.Where(n => n <= 1000).Sum(n => n);
+
+            if (numbers.Length >= 3)
+            {
+                int sum = 0;
+                //Did a for loop here because you were asking for one in the help video
+                //Could have: return numbersArray.Sum(n => n)
+                for (int i = 0; i < numbersArray.Length; i++)
+                {
+                    sum += numbersArray[i];
+                }
+                return sum;
+            }
+
+
+
+            return -1;
+        }
+        private static void CheckForNegative_ThrowException(int[] numbersArray)
+        {
+            if (numbersArray.Any(n => n < 0))
+                throw new Exception($"Negatives not allowed: {string.Join(" ", numbersArray.Where(n => n < 0))}");
+        }
+
+
+
+
+    }
+}
---
+++ Calculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        private StringCalculator calculator;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            calculator = new StringCalculator();
+        }
+        
+        [TestMethod]
+        public void TestAdd_EmptyString_ReturnsZero()
+        {
+            var result = calculator.Add("");
+
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void TestAdd_One_ReturnsOne()
+        {
+            var result = calculator.Add("1");
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestAdd_OneTwo_ReturnsThree()
+        {
+            var result = calculator.Add("1,2");
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void TestAdd_MultipleNumbers_ReturnsSix()
+        {
+            var result = calculator.Add("1,2,3");
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Negatives not allowed: -1 -2")]
+        public void TestAdd_Negative_ThrowException()
+        {
+            try
+            {
+                var result = calculator.Add("1,-1,-2");
+                Assert.Fail("Exception should have been thrown.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
+            
+        }
+
+        [TestMethod]
+        public void TestAdd_GreaterThan1000_Return1001Ignored()
+        {
+            var result = calculator.Add("1001,1");
+
+            Assert.AreEqual(1, result);
+        }
+
+
+    }
+}
